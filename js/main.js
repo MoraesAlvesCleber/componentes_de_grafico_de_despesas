8ca6517ecc6a93ffdb4240bc02a0e5b12f8225c4
Cleber Moraes <alvesmoraesb@gmail.com>
2023-01-21 12:13:00 -0300

    Estilizando tabela
    Inserindo as cores na tabela

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const chart = new Chart(ctx, {
         labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
         datasets: [{
             label: '$',
-            backgroundColor: ['red', 'green', 'black', 'blue', 'yellow', 'pink', 'purple'],
+            backgroundColor: ['hsl(10, 79%, 65%)', 'hsl(10, 79%, 65%)', ' hsl(186, 34%, 60%)', ' hsl(10, 79%, 65%)', 'hsl(10, 79%, 65%)', ' hsl(10, 79%, 65%)', ' hsl(10, 79%, 65%)'],
             data: [ 17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48]
         }],
     },
